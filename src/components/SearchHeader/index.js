68b07357532b39cd266f0a4253494af62991e21d
heebooo <1807941596@qq.com>
2018-01-17 21:29:42 +0800

    完成了SearchHeader的功能和样式

--- src/components/SearchHeader/index.js
+++ src/components/SearchHeader/index.js
@@ -8,24 +8,27 @@ class SearchHead extends PureComponent {
     render() {
         return (
             <div className="search-head">
-                <div className="search-left">
-                    <i className="icon-angle-right"></i>
+                <div className="search-left" onClick={this.goBack}>
+                    <i className="icon-chevron-left"></i>
                 </div>
-                <div className="serach-middle">
-                    <div className="search-container">
-                        <i className="icon-search"></i>
-                        <SearchInput
-                            value=""
-                            enterHandle={this.enterHandle}
-                        />
-                    </div>
+                <div className="input-container">
+                    <i className="icon-search"></i>
+                    <SearchInput
+                        value={this.props.keyword || ''}
+                        enterHandle={this.enterHandle}
+                    />
                 </div>
             </div>
         );
     };
 
+    // 返回
+    goBack = () => {
+        window.history.back();
+    };
+
+    // 跳转界面
     enterHandle = (value) => {
-        // 跳转界面
         this.props.history.push(`/search/all/${encodeURIComponent(value)}`);
     };
 }
--- src/containers/Search/index.js
+++ src/containers/Search/index.js
@@ -4,12 +4,20 @@ import SearchHead from '~components/SearchHeader';
 
 class Search extends PureComponent {
     render() {
+        const params = this.props.match.params;
         return (
             <div>
-                <SearchHead />
+                <SearchHead 
+                    keyword={params.keyword}
+                    history={this.props.history}
+                />
             </div>
         );
     };
+
+    componentDidMount() {
+        console.log(this.props)
+    }
 
 }
 
